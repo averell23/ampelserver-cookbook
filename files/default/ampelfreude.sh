a2f26562302a74f65e4717b62fdcdf43e4a3f102
Daniel Hahn <dha@betterplace.org>
2013-02-24 21:28:55 +0100

    Fixed startup script

--- files/default/ampelfreude.sh
+++ files/default/ampelfreude.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 ### BEGIN INIT INFO
-# Provides:        ntp
+# Provides:        ampelfreude
 # Required-Start:  $network $remote_fs $syslog
 # Required-Stop:   $network $remote_fs $syslog
 # Default-Start:   2 3 4 5
-# Default-Stop: 
+# Default-Stop:    0 1 6
 # Short-Description: Start NTP daemon
 ### END INIT INFO
 
@@ -19,12 +19,16 @@ case $1 in
   start)
     log_daemon_msg "Starting Ampel Runner server" "ampelfreude"
     cd $HOME
-    bundle exec ruby ampel_control.rb
+    bundle exec ruby ampel_control.rb start
     status=$?
     log_end_msg $status
       ;;
   stop)
     log_daemon_msg "Stopping NTP server" "ampelfreude"
+    cd $HOME
+    bundle exec ruby ampel_control.rb stop
+    status=$?
+    log_end_msg $status
       ;;
   restart|force-reload)
     $0 stop && sleep 2 && $0 start
